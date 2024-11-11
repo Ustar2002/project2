b82580c9149a8eb8ebf67300e09b4e2ddf554007
JYouSeong <henry7007@khu.ac.kr>
2024-11-11 14:34:40 +0900

    add star_keys

--- star.py
+++ star.py
@@ -17,19 +17,28 @@ class Star(pygame.sprite.Sprite):
         self.image = pygame.transform.scale(original_image, (30, 30))
         self.rect = self.image.get_rect(center=(x, y))
         self.speed = settings.STAR_SPEED  # settings.py에 STAR_SPEED 추가 필요
+        self.active = False  # 스타의 활성화 상태
+        self.vel = pygame.math.Vector2(0, 0)  # 스타의 속도
 
+    def set_direction(self, direction):
+        """
+        스타의 이동 방향을 설정하고 활성화합니다.
+        """
+        directions = {
+            'down': pygame.math.Vector2(0, self.speed),
+            'up': pygame.math.Vector2(0, -self.speed),
+            'left': pygame.math.Vector2(-self.speed, 0),
+            'right': pygame.math.Vector2(self.speed, 0)
+        }
+        self.vel = directions.get(direction, pygame.math.Vector2(0, self.speed))
+        self.active = True
 
-    def update(self, keys):
-        # 스타 이동 처리
-        if keys[pygame.K_UP]:
-            self.rect.y -= self.speed
-        if keys[pygame.K_DOWN]:
-            self.rect.y += self.speed
-        if keys[pygame.K_LEFT]:
-            self.rect.x -= self.speed
-        if keys[pygame.K_RIGHT]:
-            self.rect.x += self.speed
+    def update(self):
+        if self.active:
+            # 스타 이동
+            self.rect.x += self.vel.x
+            self.rect.y += self.vel.y
 
-        # 맵 경계 내에서만 이동
-        self.rect.x = max(0, min(self.rect.x, settings.MAP_WIDTH - self.rect.width))
-        self.rect.y = max(0, min(self.rect.y, settings.MAP_HEIGHT - self.rect.height))
+            # 맵 경계 내에서만 이동
+            self.rect.x = max(0, min(self.rect.x, settings.MAP_WIDTH - self.rect.width))
+            self.rect.y = max(0, min(self.rect.y, settings.MAP_HEIGHT - self.rect.height))
